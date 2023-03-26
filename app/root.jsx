fe81163e5bad15fe8a3d89cf3c2dfdcf670830be
Benjamin Meng <benjaminmeng1994@gmail.com>
2023-03-26 16:57:17 -0700

    --initial commit

--- app/root.jsx
+++ app/root.jsx
@@ -64,3 +64,14 @@ const LAYOUT_QUERY = `#graphql
     }
   }
 `;
+// const COLLECTIONS_QUERY = `#graphql
+//   query FeaturedCollections {
+//     collections(first: 3, query: "collection_type:smart") {
+//       nodes {
+//         id
+//         title
+//         handle
+//       }
+//     }
+//   }
+// `;
